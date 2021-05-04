51f88fd1f239f912e63e456ef61b61961fef9b88
Dylan Roberts <dylangardnerroberts@gmail.com>
2021-05-04 18:30:34 -0400

    Hooked up mongo db async connection in index.ts

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -24,7 +24,21 @@ app.all('*', async (req, res) => {
 
 app.use(errorHandler);
 
+const start = async () => {
+    try {
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+        })
+        console.log('Connected to MongoDB')
+    } catch (err) {
+        console.error(err)
+    }
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000!')
-});
+    app.listen(3000, () => {
+        console.log('Listening on port 3000!!')
+    });
+}
+
+start();
